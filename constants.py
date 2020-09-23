45ce4489e8811d3e470c67f35b88440d4a53033d
Eric Chow <eric.zjp.chow@gmail.com>
2020-09-22 19:53:00 -0500

    Rearrange faq msg

--- constants.py
+++ constants.py
@@ -21,6 +21,10 @@ faq_message = {
     ),
     "3. How do I enter the time?": "Please enter the time as Xam, X:XXam, XX:XXam (or pm). No space between the time and period.",
     "4. Want to add more games to the list?": "Contact chowkapow#4085",
+    "\u200b": "Advanced Commands Usage",
+    "Listing multiple players in one command": """You can add multiple players if you separate the names with spaces, e.g. __!create game time playerId1 playerId2__
+      If the name contains a space surround it with quotes, e.g. __!add n "player id__
+      This also applies with __!add__, __!remove__""",
 }
 
 game_format = {
@@ -33,34 +37,26 @@ game_format = {
 
 help_command = {
     "title": "Help Menu",
-    "description": "Schedule time to play!\nWant to contribute? [Github](https://github.com/chowkapow/play-tonight-bot)",
+    "description": "Schedule time to play games with friends!\nWant to contribute? [Github](https://github.com/chowkapow/play-tonight-bot)",
     "help_name": "**!help**",
     "help_value": "List commands",
     "faq_name": "**!faq**",
-    "faq_value": "Learn how to use this bot and what games are supported",
+    "faq_value": "Learn how to use this bot, what games are supported, and advanced commands usage",
     "create_name": "**!create**",
     "create_value": """Create a team with __!create game time__
-      __!create lol 8pm__ will create a League of Legends team with start time 8pm
-      You can add multiple players if you separate the names with spaces, e.g. __!create game time playerId1 playerId2__
-      If the name contains a space surround it with quotes, e.g. __!add n "player id__"
-      See __!faq__ for list of games""",
+      __!create lol 8pm__ will create a League of Legends team with start time 8pm""",
     "teams_name": "**!teams**",
     "teams_value": """Show current teams
-    You can filter teams by game using __!teams game__
-    See __!faq__ for list of games""",
+    You can filter teams by game using __!teams game__""",
     "join_name": "**!join**",
     "join_value": "Join team with __!join n__, where n is team number",
     "leave_name": "**!leave**",
     "leave_value": """Leave team with __!leave n__, where n is team number
     Team will be deleted if there are no more players""",
     "add_name": "**!add**",
-    "add_value": """Add a player with __!add n playerId__, where n is team number
-      You can add multiple players if you separate the names with spaces, e.g. __!add n playerId1 playerId2__
-      If the name contains a space surround it with quotes, e.g. __!add n "player id__\"""",
+    "add_value": "Add a player with __!add n playerId__, where n is team number",
     "remove_name": "**!remove**",
-    "remove_value": """Remove a player with __!remove n playerId__, where n is team number
-      You can remove multiple players if you separate the names with spaces, e.g. __!remove n playerId1 playerId2__
-      If the name contains a space surround it with quotes, e.g. __!remove n "player id__\"""",
+    "remove_value": "Remove a player with __!remove n playerId__, where n is team number",
     "edit_name": "**!edit**",
     "edit_value": """Edit time to play with __!edit n time__, where n is team number
       __!edit 2 9pm__ will change Team 2's start time to 9pm
